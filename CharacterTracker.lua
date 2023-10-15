405fcaa6f76a89822633398124fbe6f62258469d
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2023-10-15 19:37:15 +0100

    CharacterTracker
    Character Tracker with player mouse event.

---
+++ CharacterTracker.lua
@@ -0,0 +1,31 @@
+local Players = game:GetService("Players")
+local player = game.Players.LocalPlayer
+local playerMouse = player:GetMouse()
+local Target = playerMouse.Target
+local Highlight = Instance.new("Highlight")
+
+---When the RopeTool is put on by the client
+local function OnRopeToolActivated()
+
+    --->Move event to update the Target Property
+playerMouse.Move:Connect(function()
+    Target = playerMouse.Target
+end)
+
+    while true do
+        task.wait()  
+        if Target and Target.Parent:FindFirstChild("Humanoid") then
+            local CharacterModel = Target.Parent --Every character with a Humanoid is set to the CharacterModel Variable
+            Highlight.Parent = CharacterModel
+            warn("FOUND A CHARACTER")
+        else
+            Highlight.Parent = nil
+            print("DID NOT FIND A CHARACTER")
+            end   
+    end
+
+
+end
+
+--->LAUNCHES
+OnRopeToolActivated()
