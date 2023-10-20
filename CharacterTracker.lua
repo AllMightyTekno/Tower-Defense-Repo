f91a1b6285999e3ed0dd37c95a9db0fd7388e4bb
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2023-10-20 19:27:40 +0100

    Update CharacterTracker.lua

--- CharacterTracker.lua
+++ CharacterTracker.lua
@@ -6,7 +6,7 @@ local Target = playerMouse.Target
 local Highlight = Instance.new("Highlight")
 
 ---When the RopeTool is put on by the client
-local function OnRopeToolActivated()
+local function TrackCharacterLauncher()
 
     --->Move event to update the Target Property
 playerMouse.Move:Connect(function()
@@ -14,7 +14,7 @@ playerMouse.Move:Connect(function()
 end)
 
     while true do
-        task.wait()  
+        task.wait()     
         if Target and Target.Parent:FindFirstChild("Humanoid") then
             local CharacterModel = Target.Parent --Every character with a Humanoid is set to the CharacterModel Variable
             Highlight.Parent = CharacterModel
@@ -29,4 +29,4 @@ end)
 end
 
 --->LAUNCHES
-OnRopeToolActivated()
+TrackCharacterLauncher()
