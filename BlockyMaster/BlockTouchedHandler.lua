63190fa0aedc15e44cfe8a61a0c12bd4403407cc
Wboy0010 <143533874+AllMightyTekno@users.noreply.github.com>
2024-01-17 02:08:57 +0100

    New and Improved Block Framework

--- BlockyMaster/BlockTouchedHandler.lua
+++ BlockyMaster/BlockTouchedHandler.lua
@@ -1,56 +1,49 @@
-local TS = game:GetService("TweenService")
-local BlockModel = game.Workspace.BLOCK 
-local Triggers = game.Workspace.Triggers:GetChildren("Base")
-local TriggerGateModels = game.Workspace:FindFirstChild("Triggers")
+-->Services
+local TweenService = game:GetService("TweenService")
 
- for _, Triggers in pairs(game.Workspace.Triggers:GetChildren()) do
+-->Variables
+local BlockModel = Workspace.BLOCK
+local Triggers = Workspace.Triggers:GetChildren("Base")
+local TriggerGateModels = Workspace:FindFirstChild("Triggers")
+
+
 	
 	
-BlockModel.Touched:Connect(function(hit)
-		if hit and hit:IsDescendantOf(game.Workspace.Triggers) then --Checks wheter what touched the trigger is a chikld of the triggers folder
-    --FIRES WHEN THE BLOCK TOUCHES THE TRIGGERS
+BlockModel.Touched:Connect(function(hit: BasePart)
+		if hit:IsDescendantOf(game.Workspace.Triggers) then return end --Checks wheter what touched the trigger is a chikld of the triggers folder
+    --IF BLOCK TOUCHES TRIGGER>>>
 local TouchedTrigger = hit
 local ToucehdTriggerDoorModel = hit:GetAttribute("Trigger_Number")  --Get's the Attribute of the gate that's suppose to be opended by the player
 			
 			
-			if TriggerGateModels then --If the TriggersGate Model in the Triggers model is found.
+			if TriggerGateModels then return end  --If the TriggersGate Model in the Triggers model is found.
 				
-				for _, part in pairs(TriggerGateModels:GetDescendants()) do
+				for _,part: BasePart in pairs(TriggerGateModels:GetDescendants()) do
 					
-					if part.Name == "Gate" and ToucehdTriggerDoorModel == part:GetAttribute("GateNumber") then
+					if part.Name ~= "Gate" and ToucehdTriggerDoorModel ~= part:GetAttribute("GateNumber") then continue end
 						
 						local partOriginalPosition = part.Position
 						local offset = Vector3.new(0, 0, 20) -- Adjust the offset as needed
 						local newCFrame = part.CFrame * CFrame.new(offset)
 						
-						local OpenTween = TS:Create(part,TweenInfo.new(6), {CFrame = newCFrame})
+						local OpenTween = TweenService:Create(part,TweenInfo.new(6), {CFrame = newCFrame})
 							
-						OpenTween:Play()
+						OpenTween:Play() --Opens the Door 
 						part.Parent.Cross.Base.CanTouch = false
 						task.wait(10)
 						
-						if OpenTween.Completed then --if the door's tween has finished then it tweens back to it's normal position
-						
+						 OpenTween.Completed:Wait() --if the door's tween has finished then it tweens back to it's normal position
+
 							local offset = Vector3.new(0, 0, -20) -- Adjust the offset as needed
 							local newCFrame = part.CFrame * CFrame.new(offset)
-							local CloseTween = TS:Create(part,TweenInfo.new(6), {CFrame = newCFrame})
-
+							local CloseTween = TweenService:Create(part,TweenInfo.new(6), {CFrame = newCFrame})
 							CloseTween:Play()
-							if CloseTween.Completed then
-								part.Parent.Cross.Base.CanTouch = true
-							end
-						end
-						
-						
-					end
+
+							 CloseTween.Completed:Wait() 
+						     part.Parent.Cross.Base.CanTouch = true
+							
+									
 					
-				end
-				
-			end
-			
-			
-       end
+				end --end of for loop		
 
    end)
-
-end
